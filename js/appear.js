a880c42a992859de6f5e8b23df452bff54578bad
unknown <marcin.adam.mroczko@gmail.com>
2019-02-08 22:38:45 +0100

    decreasing speed of appearing

--- js/appear.js
+++ js/appear.js
@@ -1,6 +1,6 @@
 window.sr=ScrollReveal({reset:!0}),
 sr.reveal('#appear-left', {duration: 250, origin: 'left', opacity: 0.3, distance: '300px'});
 sr.reveal('#appear-right', {duration: 250, origin: 'right', opacity: 0.3, distance: '500px'});
-sr.reveal('#appear-bottom-first', {duration: 500, origin: 'bottom', opacity: 0, distance: '100px', delay: 100});
-sr.reveal('#appear-bottom-second', {duration: 500, origin: 'bottom', opacity: 0, distance: '100px', delay: 150});
-sr.reveal('#appear-bottom-third', {duration: 500, origin: 'bottom', opacity: 0, distance: '100px', delay: 200});
+sr.reveal('#appear-bottom-first', {duration: 300, origin: 'bottom', opacity: 0, distance: '100px', delay: 100});
+sr.reveal('#appear-bottom-second', {duration: 300, origin: 'bottom', opacity: 0, distance: '100px', delay: 150});
+sr.reveal('#appear-bottom-third', {duration: 300, origin: 'bottom', opacity: 0, distance: '100px', delay: 200});
